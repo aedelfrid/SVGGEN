e795a9c5f2ff891c0402d9c04c8c79553415a6ff
aedelfrid <almck51@gmail.com>
2023-07-28 22:33:07 -0300

    added some unit tests and began fs writeToFile test

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const {questions, askQuestion} = require('./lib/inquirerUtil')
+const inquirer = require('inquirer')
+
+const  { questions } = require('./lib/questions')
+
+const { renderShape, renderSVG, writeToFile } = require('./lib/generation')
+
+const askQuestion = async (questions) => {
+    inquirer.prompt(questions)
+        .then((answers) => {
+
+            if (answers.start === true) {
+                console.log(answers)
+
+                const shape = renderShape(answers);
+
+                const SVG = renderSVG(shape)
+                
+                writeToFile(SVG.toString())
+                console.log('Generated logo.svg')
+            } else {
+                console.log('SVG GEN Aborted.')
+            }
+        })
+};
 
 askQuestion(questions);
